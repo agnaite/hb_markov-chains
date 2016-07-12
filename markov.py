563683c8e4029d27f2b1993e6dfcb4dc7e7a5cfe
Hackbright Student <info@hackbrightacademy.com>
2016-07-12 12:50:54 -0700

    removed .keys() methods

--- markov.py
+++ markov.py
@@ -8,12 +8,10 @@ def open_and_read_file(file_path):
     the file's contents as one string of text.
     """
 
-    # your code goes here
     contents = open(file_path)
     contents_read = contents.read()
     text = contents_read.split()
     contents.close()
-
 
     return text
 
@@ -30,10 +28,8 @@ def make_chains(text_string):
         {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
     """
 
-
     chains = {}
 
-    # your code goes here
     #loops through the text string until it reaches the second to last word
     for index in range(0, len(text_string) - 2): 
         #defines variables for words based on their position relative to the indexed word
@@ -44,7 +40,7 @@ def make_chains(text_string):
         #if a key with a tuple of the first and second words is already in the dict,
         #appends the third word to the list already in place as the value.
         #else, creates the key with the third word in a list.
-        if (first_word, second_word) in chains.keys():
+        if (first_word, second_word) in chains:
             chains[(first_word, second_word)].append(third_word)
         else:
             chains[(first_word, second_word)] = [third_word]
@@ -59,17 +55,15 @@ def make_text(chains):
 
     current_key = choice(chains.keys())
 
-    while current_key in chains.keys():
+    while current_key in chains:
         current_value = choice(chains[current_key])
         text += current_value + " "
         current_key = (current_key[1], current_value)
 
-        # your code goes here
-
     return text
 
 
-input_path = "green-eggs.txt"
+input_path = "gettysburg.txt"
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
